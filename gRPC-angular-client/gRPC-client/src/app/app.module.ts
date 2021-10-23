c3c5721171f32be782113917d1928dc653b3e64d
Furbyv <fbverbeek@gmail.com>
2021-10-23 15:58:05 +0200

    Add angular material to front-end

--- gRPC-angular-client/gRPC-client/src/app/app.module.ts
+++ gRPC-angular-client/gRPC-client/src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WozObjectTest } from './wozObjectTest/wozObjectTest.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, WozObjectTest],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
