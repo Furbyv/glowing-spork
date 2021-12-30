98e1343fd3a86882fe8abc87ca5914cc87f1c93a
Furbyv <fbverbeek@gmail.com>
2021-12-30 11:47:18 +0100

    start objectproperties card

--- gRPC-angular-client/gRPC-client/src/app/features/object-search/components/object-details/object-properties/object-properties.component.ts
+++ gRPC-angular-client/gRPC-client/src/app/features/object-search/components/object-details/object-properties/object-properties.component.ts
@@ -1,4 +1,13 @@
-import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
+import { ReplaySubject, Subject } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+import { GetFullWozObjectService } from '../../../services/get-full-object.service';
 
 @Component({
   selector: 'app-object-properties',
@@ -6,6 +15,24 @@ import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
   styleUrls: ['./object-properties.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ObjectPropertiesComponent {
+export class ObjectPropertiesComponent implements OnChanges {
   @Input() id: number | undefined = 0;
+  refresh$$: Subject<boolean> = new ReplaySubject<boolean>(1);
+
+  loading$ = this.getFullWozObjectService.fullWozObject$.pipe(
+    map((state) => state.loading)
+  );
+
+  wozObject$ = this.getFullWozObjectService.fullWozObject$.pipe(
+    filter((state) => state.success),
+    map((state) => state.res!)
+  );
+
+  constructor(private getFullWozObjectService: GetFullWozObjectService) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.id && this.id) {
+      this.getFullWozObjectService.getFullWozObject(this.id);
+    }
+  }
 }
