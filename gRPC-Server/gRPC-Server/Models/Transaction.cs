d8bddd12f1f44cd5d6b0433c5ff4b45eed7e9d15
Furbyv <fbverbeek@gmail.com>
2022-04-19 19:33:02 +0200

    Datamodel transactions

---
+++ gRPC-Server/gRPC-Server/Models/Transaction.cs
@@ -0,0 +1,14 @@
+﻿namespace gRPCServer.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public long VolgnummerMarktgegeven { get; set; }
+        public long Wozobjectnummer { get; set; }
+        public string Aard { get; set; }
+        public string Soort { get; set; }
+        public string Bruikbaarheid { get; set; }
+        public DateTime TrasactieDatum {  get; set; }
+        public long Transactieprijs { get; set; }
+    }
+}
