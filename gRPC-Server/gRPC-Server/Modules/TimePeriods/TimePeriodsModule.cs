1afae166986c67fdab9e204bc05837becec2bc4c
Furbyv <fbverbeek@gmail.com>
2022-06-13 13:52:53 +0200

    move timeperiod config to module

---
+++ gRPC-Server/gRPC-Server/Modules/TimePeriods/TimePeriodsModule.cs
@@ -0,0 +1,14 @@
+﻿namespace gRPCServer.Modules.TimePeriods;
+public class TimePeriodsModule : IModule
+{
+    public IEndpointRouteBuilder MapEndpoints(IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapGrpcService<TimePeriodService>().EnableGrpcWeb().RequireCors("AllowAll");
+        return endpoints;
+    }
+
+    public IServiceCollection RegisterModule(IServiceCollection builder)
+    {
+        return builder;
+    }
+}
--- gRPC-Server/gRPC-Server/Program.cs
+++ gRPC-Server/gRPC-Server/Program.cs
@@ -1,9 +1,11 @@
 using gRPCServer;
+using gRPCServer.Modules;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEntityFrameworkNpgsql().AddDbContext<DataContext>(optionsBuilder =>
 optionsBuilder.UseNpgsql(builder.Configuration.GetConnectionString("dbContext"), x => x.UseNetTopologySuite()).ReplaceService<ISqlGenerationHelper, CustomNameSqlGenerationHelper>().EnableDetailedErrors());
 builder.Services.AddGrpc();
+builder.Services.RegisterModules();
 builder.Services.AddCors(o => o.AddPolicy("AllowAll", builder =>
 {
     builder.AllowAnyOrigin()
@@ -16,6 +18,7 @@ var app = builder.Build();
 app.UseRouting();
 app.UseGrpcWeb();
 app.UseCors();
+app.MapEndpoints();
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapGrpcService<WozObjectService>().EnableGrpcWeb().RequireCors("AllowAll");
@@ -23,7 +26,6 @@ app.UseEndpoints(endpoints =>
     endpoints.MapGrpcService<TaxOverviewService>().EnableGrpcWeb().RequireCors("AllowAll");
     endpoints.MapGrpcService<TransactionService>().EnableGrpcWeb().RequireCors("AllowAll");
     endpoints.MapGrpcService<NoteService>().EnableGrpcWeb().RequireCors("AllowAll");
-    endpoints.MapGrpcService<TimePeriodService>().EnableGrpcWeb().RequireCors("AllowAll");
     endpoints.MapGrpcService<TaxationService>().EnableGrpcWeb().RequireCors("AllowAll");
 }
 );
